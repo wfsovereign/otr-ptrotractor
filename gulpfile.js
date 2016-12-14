2011a6911d6d1d957dd3dd27a3ac1d322f625eee
wfsovereign <wfsovereign@outlook.com>
2016-12-14 19:55:37 +0800

    update input and output directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 const gulp = require('gulp');
 const request = require('request');
 const fs = require('fs');
+const path = require('path');
 const decompress = require('gulp-decompress');
 const fileUrl = 'https://github.com/wfsovereign/protractor-dump/blob/master/node_modules.zip?raw=true';
+const inputDir = path.join(__dirname, 'dump.zip');
+const outputDir = path.join(__dirname, 'node_modules');
+
 const downloadZipFile = function () {
   return new Promise(function (resolve, reject) {
     request(fileUrl)
       .pipe(fs.createWriteStream('dump.zip'))
       .on('drain', function (chunk) {
-          console.log('chunk');
+        console.log('chunk');
       })
       .on('open', function (chunk) {
-          console.log('open chunk :', chunk);
+        console.log('open chunk :', chunk);
       })
       .on('close', function () {
         console.log('File written!');
+        console.log('input dir : ', inputDir);
         resolve();
       });
   });
@@ -22,9 +27,10 @@ const downloadZipFile = function () {
 
 gulp.task('unzip', function (done) {
   downloadZipFile().then(function (data) {
-    gulp.src('./dump.zip')
-    .pipe(decompress({strip: 1}))
-    .pipe(gulp.dest('./node_modules'));
+    gulp.src(inputDir)
+      .pipe(decompress({ strip: 1 }))
+      .pipe(gulp.dest(outputDir));
+    console.log('output dir : ', outputDir);
     console.log('success unzip!');
     done();
   });
