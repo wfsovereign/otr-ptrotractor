543bb34d0db6ce702afe8e33211ebf23505e26ed
wfsovereign <wfsovereign@outlook.com>
2016-12-15 21:09:22 +0800

    update gulp file to minimize this project

--- README.md
+++
@@ -1,5 +0,0 @@
-
-due to protractor too big so create this repo to install protractor.
-
-
-protractor version : 4.0.10
--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,18 @@ const gulp = require('gulp');
 const request = require('request');
 const fs = require('fs');
 const path = require('path');
+const mv = require('mv');
 const decompress = require('gulp-decompress');
-const fileUrl = 'https://coding.net/u/wfsovereign/p/file-dump/git/raw/master/node_modules.zip';
+const fileUrl = 'https://coding.net/u/wfsovereign/p/file-dump/git/raw/master/selenium.zip';
 const inputDir = path.join(__dirname, 'dump.zip');
-const outputDir = path.join(__dirname, 'node_modules');
+const outputDir = path.join(__dirname, 'node_modules/selenium/');
 
 const downloadZipFile = function () {
   let index = 0;
   return new Promise(function (resolve, reject) {
     request(fileUrl)
       .on('error', function (error) {
-          console.log('pre error :', error);
+        console.log('pre error :', error);
       })
       .pipe(fs.createWriteStream('dump.zip'))
       .on('drain', function (chunk) {
@@ -33,12 +34,33 @@ const downloadZipFile = function () {
 };
 
 gulp.task('unzip', function (done) {
+  console.log('current dir: ', __dirname);
+  const targetDir = path.join(__dirname, 'node_modules/protractor');
+  const targetDestDir = __dirname;
+
+  function moveAllFileByPath(targetDir, targetDestDir) {
+    const allFile = fs.readdirSync(targetDir);
+    allFile.forEach(ele => {
+      console.log('move file :', ele);
+      if (ele === 'gulpfile.js') return;
+      if (ele === 'node_modules' || ele === '.bin') {
+        moveAllFileByPath(path.join(targetDir, ele), path.join(targetDestDir, ele));
+      } else {
+        mv(path.join(targetDir, ele), path.join(targetDestDir, ele), { mkdirp: true }, function (err) {
+          console.log('error : ', err);
+          console.log('finished!');
+        });
+      }
+    });
+  }
+
   downloadZipFile().then(function (data) {
     gulp.src(inputDir)
       .pipe(decompress({ strip: 1 }))
       .pipe(gulp.dest(outputDir));
     console.log('output dir : ', outputDir);
     console.log('success unzip!');
+    moveAllFileByPath(targetDir, targetDestDir);
     done();
   });
 });
