b9183c97d65f1908e2bcfb9033ae6bdcc53db15e
wfsovereign <wfsovereign@outlook.com>
2016-12-15 15:21:50 +0800

    restore unzip task and update dump source uri

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fs = require('fs');
 const path = require('path');
 const decompress = require('gulp-decompress');
-const fileUrl = 'https://github.com/wfsovereign/protractor-dump/blob/master/node_modules.zip?raw=true';
+const fileUrl = 'https://coding.net/u/wfsovereign/p/file-dump/git/raw/master/node_modules.zip';
 const inputDir = path.join(__dirname, 'dump.zip');
 const outputDir = path.join(__dirname, 'node_modules');
 
@@ -33,13 +33,12 @@ const downloadZipFile = function () {
 };
 
 gulp.task('unzip', function (done) {
-  // downloadZipFile().then(function (data) {
-  //   gulp.src(inputDir)
-  //     .pipe(decompress({ strip: 1 }))
-  //     .pipe(gulp.dest(outputDir));
-  //   console.log('output dir : ', outputDir);
-  //   console.log('success unzip!');
-  //   done();
-  // });
-  console.log('finished mock unzip.');
+  downloadZipFile().then(function (data) {
+    gulp.src(inputDir)
+      .pipe(decompress({ strip: 1 }))
+      .pipe(gulp.dest(outputDir));
+    console.log('output dir : ', outputDir);
+    console.log('success unzip!');
+    done();
+  });
 });
